49bde21c3dc6092f3f0de4dedd1f4f7eb3491f15
Vinicius Da Silva Lima <vinda-si@c1r6p4.42sp.org.br>
2026-03-07 13:58:18 -0300

    fix error

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 15:09:59 by vinda-si          #+#    #+#             */
-/*   Updated: 2025/12/28 17:19:41 by vinda-si         ###   ########.fr       */
+/*   Updated: 2026/03/07 13:51:06 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ DiamondTrap::DiamondTrap(std::string name)
 	this->_name = name;
 
 	this->_hitPoints = FragTrap::_hitPoints;
-	this->_energyPoints = ScavTrap::_energyPoints;
+	this->_energyPoints = 50;
 	this->_attackDamage = FragTrap::_attackDamage;
 
 	std::cout << "DiamondTrap " << this->_name << " constructed!" << std::endl;
@@ -30,10 +30,27 @@ DiamondTrap::DiamondTrap() : ClapTrap("Default_Clap_Name"), ScavTrap(), FragTrap
 {
 	this->_name = "Default";
 	this->_hitPoints = FragTrap::_hitPoints;
-	this->_energyPoints = ScavTrap::_energyPoints;
+	this->_energyPoints = 50;
 	this->_attackDamage = FragTrap::_attackDamage;
 
 	std::cout << "DiamondTrap Default constructed!" << std::endl;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap &src) : ClapTrap(src), ScavTrap(src), FragTrap(src)
+{
+	*this = src;
+	std::cout << "DiamondTrap " << this->_name << " copy constructed!" << std::endl;
+}
+
+DiamondTrap &DiamondTrap::operator=(const DiamondTrap &src)
+{
+	std::cout << "DiamondTrap copy assignment operator called!" << std::endl;
+	if (this != &src)
+	{
+		ClapTrap::operator=(src);
+		this->_name = src._name;
+	}
+	return *this;
 }
 
 DiamondTrap::~DiamondTrap()
